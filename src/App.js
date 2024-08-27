ed8a2ed6f214a116dfd94312e771274770cbf92e
Pardhu Guttula <Pardhu.Guttula@BRILLIO.COM>
2024-08-27 12:14:27 +0530

    modified App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to My React App</h1>
+        <h1>Welcome to React App</h1>
         <p>This is a basic React app.</p>
       </header>
     </div>
