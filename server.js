541026d22e8bbbd274dea29a234ce533eac34178
Pardhu Guttula <Pardhu.Guttula@BRILLIO.COM>
2024-09-02 14:37:04 +0530

    build

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const path = require("path");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const app = express();
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "build")));
 
-// Handle any requests that don't match the static files
+// Set up reverse proxy for /feature route
+app.use(
+  "/feature",
+  createProxyMiddleware({
+    target: "http://172.206.251.171:5000",
+    changeOrigin: true,
+  })
+);
+
+// Handle any other requests by serving the React app
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
