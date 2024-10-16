4c86eb6408d2210002131893f44d1e452c85b591
Pardhu Guttula <87533027+Pardhu-Guttula@users.noreply.github.com>
2024-10-16 21:37:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to React App</h1>
-        <p>This is a basic React app. Now we are deploying it to staging</p>
+        <p>This is a basic React app. Now we are deploying it to staging env</p>
       </header>
     </div>
   );
